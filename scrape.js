634185b8318af073dd6d5cedb194301fd1191fbb
Sean <sean_m_fitzgerald@tutanota.com>
2021-01-29 18:06:29 -0800

    added getQuotes

--- scrape.js
+++ scrape.js
@@ -3,11 +3,33 @@ const cheerio = require("cheerio");
 const base_url = "https://fallout.fandom.com";
 const sqlite = require("sqlite3");
 
-async function getPages(pages, url) {
+function getQuotes(url) {
+	// tags of interest
+	// ================
+	// "div.mediaContainer" - holds the audio file player
+	//
+	// "li" - in "Notable Quotes" section, contains quote and "div.np-quote-snd-player"
+	// "div.np-quote-snd-player" contains href to audio file.
+	//
+	// "span.va-quotation-text" - top-of-page quote
+	//
+	// "div.lightbox-caption" - contains a quote and a "div.mediaContainer"
+
+}
+
+async function crawl(quotes, url) {
 
 	if (url.split("/").find(str => str.startsWith("Category:")) === undefined) {
 		console.log(`Found article: ${url}`)
-		pages.push(url);
+		const page_quotes = getQuotes(url);
+		for (q in page_quotes) {
+			quotes.push({
+				"text": q["text"],
+				"audio": q["audio"],
+				"url": url
+			});
+		}
+		console.log(`Got ${page_quotes.length}`);
 		return;
 	}
 
@@ -17,18 +39,16 @@ async function getPages(pages, url) {
 	var members = $("div.category-page__members").find(".category-page__member-link");
 	var i = 0
 	while (members[i]) {
-		getPages(pages, members[i].attribs["href"]);
+		crawl(quotes, members[i].attribs["href"]);
 		i++;
 	}
 }
 
-function getCharList() {
+function scrape() {
 	const start_url ="/wiki/Category:Characters_by_game";
 	const pages = [];
-	getPages(pages, start_url);
+	crawl(pages, start_url);
 	return pages;
 }
 
-function findQuotes
-
-const charList = getCharList();
+const pages = scrape();
